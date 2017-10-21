e055008a8965543bc26a4975afc8039dbb36c5d5
Kenneth Chau <Kenneth.Chau@massive.co>
2017-10-21 16:45:00 +0100

    progress bar click

--- js/main.js
+++ js/main.js
@@ -7,27 +7,73 @@ function initPage(times, images) {
     /* Variables */
     var currentTag = 0;
     var allTags = [];
-
-    var audio = document.getElementById("sound");
-    var timeline = document.getElementById("timeline");
-    var mainButton = document.getElementById("mainButton");
-    var backButton = document.getElementById("backButton");
-    var forwardButton = document.getElementById("forwardButton");
-
-    // initialise variables
+    // initialise all tabs
     for (i=0; i < times.length; i++) {
         allTags.push({
             time: times[i],
             image: images[i]
         });
     }
-    
+
+    var audio = document.getElementById("sound");
+    var mainButton = document.getElementById("mainButton");
+    var backButton = document.getElementById("backButton");
+    var forwardButton = document.getElementById("forwardButton");
+    var timeline = document.getElementById("timeline");
+
     /* Events */
     audio.addEventListener("timeupdate", handleTimeUpdate);
-    timeline.addEventListener("click", handleTimelineClick);
     mainButton.addEventListener("click", handleMainButtonClick);
-    backButton.addEventListener("click", skipBackward);
-    forwardButton.addEventListener("click", skipForward);
+    backButton.addEventListener("click", handleSkipBackward);
+    forwardButton.addEventListener("click", handleSkipForward);
+    timeline.addEventListener("click", handleTimelineClick);
+
+    /* Event Handler */
+    function handleTimeUpdate() {
+        updateProgressBar();
+        if (currentTag >= allTags.length - 1) return;
+        var nextTag = allTags[currentTag + 1]
+        if (audio.currentTime > nextTag.time)
+            updateIllustrationContent(nextTag.image);
+    }
+
+    function handleMainButtonClick() {
+        if (audio.paused) {
+            play();
+        }
+        else {
+            pause();
+        }
+    }
+
+    function handleSkipBackward() {
+        if (currentTag < 1) return;
+        var newTag = allTags[currentTag - 1]
+        audio.currentTime = newTag.time;
+        updateIllustrationContent(newTag.image);
+        currentTag = currentTag - 1;
+    }
+
+    function handleSkipForward() {
+        if (currentTag >= allTags.length - 1) return;
+        var newTag = allTags[currentTag + 1]
+        audio.currentTime = newTag.time;
+        updateIllustrationContent(newTag.image);
+        currentTag = currentTag + 1;
+    }
+
+    function handleTimelineClick(e) {
+        console.log(audio.currentTime);
+        console.log(e.layerX);
+        console.log(e.target.clientWidth);
+        console.log(audio.duration);
+        var newTime = (e.layerX/e.target.clientWidth) * audio.duration;
+        console.log(newTime);
+        console.log(audio.duration);
+        audio.currentTime = newTime;
+        console.log(audio.currentTime);
+        updateProgressBar();
+    }
 
     /* Functions */
     function play() {
@@ -40,51 +86,10 @@ function initPage(times, images) {
         mainButton.innerText = "Play"
     }
 
-    function skipBackward() {
-        if (currentTag < 1) return;
-        var newTag = allTags[currentTag - 1]
-        audio.currentTime = newTag.time;
-        updateIllustrationContent(newTag.image);
-        currentTag = currentTag - 1;
-    }
-
-    function skipForward() {
-        if (currentTag >= allTags.length - 1) return;
-        var newTag = allTags[currentTag + 1]
-        audio.currentTime = newTag.time;
-        updateIllustrationContent(newTag.image);
-        currentTag = currentTag + 1;
-    }
-
     function updateProgressBar() {
         var percentage = (audio.currentTime/audio.duration) * 100;
         var playhead = document.getElementById("playhead");
         playhead.style.left = "calc(" + percentage + "% - 15px";
-    }
-
-    function seek(time) {
-
-    }
-
-    function handleTimelineClick() {
-
-    }
-
-    function handleMainButtonClick() {
-        if (audio.paused) {
-            play();
-        }
-        else {
-            pause();
-        }
-    }
-
-    function handleTimeUpdate(e) {
-        updateProgressBar();
-        if (currentTag >= allTags.length - 1) return;
-        var nextTag = allTags[currentTag + 1]
-        if (audio.currentTime > nextTag.time)
-            updateIllustrationContent(nextTag.image);
     }
 
     function updateIllustrationContent(content) {
